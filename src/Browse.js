f1f20c2419164bcae065fa968feced9090697879
Russell Tennant <rtennant@confederationcollege.ca>
2021-04-10 19:17:10 -0400

    Made navbar fixed and menus slide in from the right. Added browse js and css files.

---
+++ src/Browse.js
@@ -0,0 +1,14 @@
+import './css/Browse.css';
+import React from 'react';
+
+class Browse extends React.Component {
+  render() {
+    return (
+      <main>
+
+      </main>
+    );
+  }
+}
+
+export default Browse;
