31856d2731f4fed0d3de51c6c5dbd3dcefc16dbd
Russell Tennant <rtennant@confederationcollege.ca>
2021-04-10 15:06:06 -0400

    Added main section

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './images/noodlewall-logo-50p.png';
 import './css/App.css';
 import React from 'react';
 import Navbar from './Navbar.js';
+import Landing from './Landing.js';
 import Footer from './Footer.js';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Landing/>
       <Footer/>
     </div>
   );
---
+++ src/Landing.js
@@ -0,0 +1,14 @@
+import './css/Landing.css';
+import React from 'react';
+
+class Landing extends React.Component {
+  render() {
+    return (
+      <main>
+        Content goes here
+      </main>
+    );
+  }
+}
+
+export default Landing;
