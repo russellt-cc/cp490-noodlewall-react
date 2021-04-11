b82da6bb917e6f6e5f9fa3aa02d125c3fcba5827
Russell Tennant <rtennant@confederationcollege.ca>
2021-04-11 00:40:38 -0400

    Added card view to browse page

--- src/Noodles.js
+++ src/Noodles.js
@@ -3,9 +3,10 @@
 //https://www.pluralsight.com/guides/convert-a-json-file-to-an-array-in-react
 
 // Get local JSON file
-import {noodleData} from "./noodleData.js";
+import {noodleData} from "./noodleData.js"
 import React from 'react';
 import {Link} from "react-router-dom"
+import fishing from "./images/fishing-crop.png"
 
 // The list of noodles from the JSON data
 // Could be modified to pull data from a database
@@ -27,18 +28,61 @@ class NoodleList extends React.Component {
 // Class to structure the data for each noodle
 class Noodle extends React.Component {
     render() {
+        // Get the noodle data from the props
         const data = this.props.data
         const noodleID = data.noodleID
-        const userID = data.noodleID
+        const userID = data.userID
         const status = data.noodleStatus
+        const userRating = data.userRating
+        // Make an array to store the star classes
+        var star_classes = ["", "", "", "", ""]
+        // Determine which ones are checked based on the rating
+        switch (userRating) {
+            case 5:
+                star_classes[5] = "checked"
+                star_classes[4] = "checked"
+                star_classes[3] = "checked"
+                star_classes[2] = "checked"
+                star_classes[1] = "checked"
+                break
+            case 4:
+                star_classes[4] = "checked"
+                star_classes[3] = "checked"
+                star_classes[2] = "checked"
+                star_classes[1] = "checked"
+                break
+            case 3:
+                star_classes[3] = "checked"
+                star_classes[2] = "checked"
+                star_classes[1] = "checked"
+                break
+            case 2:
+                star_classes[2] = "checked"
+                star_classes[1] = "checked"
+                break
+            case 1:
+            default:
+                star_classes[1] = "checked"
+                break
+        }
         return(
             // div for each noodle
             <div className={`noodle ${status}`}>
+                <Link class="noodle_image_link" to={`/details/${noodleID}`}><img src={fishing} alt="Noodle"></img></Link>
                 <p className={`noodle_id`}><span className="noodle_label">ID: </span>{noodleID}</p>
                 <p className={`noodle_title`}><span className="noodle_label">Title: </span><Link to={`/details/${noodleID}`}>{data.noodleTitle}</Link></p>
                 <p className={`noodle_status`}><span className="noodle_label">Status: </span>{status}</p>
-                <p className={`noodle_userid`}><span className="noodle_label">User ID: </span>{userID}</p>
-                <p className={`noodle_noodler`}><span className="noodle_label">Noodler: </span><Link to={`/user/${userID}`}>{data.noodlerName}</Link></p>
+                <div className={`noodle_userinfo`}>
+                    <p className={`noodle_userid`}><span className="noodle_label">User ID: </span>{userID}</p>
+                    <p className={`noodle_noodler`}><span className="noodle_label">Noodler: </span><Link to={`/user/${userID}`}>{data.userName}</Link></p>
+                    <p class="user_rating">
+                        <span className={`fa fa-star ${star_classes[1]}`}></span>
+                        <span className={`fa fa-star ${star_classes[2]}`}></span>
+                        <span className={`fa fa-star ${star_classes[3]}`}></span>
+                        <span className={`fa fa-star ${star_classes[4]}`}></span>
+                        <span className={`fa fa-star ${star_classes[5]}`}></span>
+                    </p>
+                </div>
                 <p className={`noodle_description`}><span className="noodle_label">Description: </span>{data.noodleDescription}</p>
                 <div className={`noodle_tags_section`}>
                     <p className="noodle_label">Tags:</p>
--- src/noodleData.js
+++ src/noodleData.js
@@ -3,45 +3,118 @@ export var noodleData = [
         "noodleID":1,
         "noodleTitle":"Fishing with Friends",
         "userID":1,
-        "noodlerName":"Pam Poovey",
+        "userName":"Pam Poovey",
+        "userRating":4,
         "noodleStatus":"dream",
         "noodleDescription":"Hey, I am Pam! I love fishing but none of my friends do! I have a boat, and all the gear you could possibly need. Fish is on the menu but if we get skunked, I have food available as well.",
-        "noodleTags":["fishing", "food", "hobby", "friends", "sports"]
+        "noodleTags":["fishing", "food", "hobby", "friends", "sports"],
+        "noodleImage":"fishing"
     },
     {
         "noodleID":2,
         "noodleTitle":"Fishing with Friends",
         "userID":1,
-        "noodlerName":"Pam Poovey",
+        "userName":"Pam Poovey",
+        "userRating":4,
         "noodleStatus":"dream",
         "noodleDescription":"Hey, I am Pam! I love fishing but none of my friends do! I have a boat, and all the gear you could possibly need. Fish is on the menu but if we get skunked, I have food available as well.",
-        "noodleTags":["fishing", "food", "hobby", "friends", "sports"]
+        "noodleTags":["fishing", "food", "hobby", "friends", "sports"],
+        "noodleImage":"fishing"
     },
     {
         "noodleID":3,
         "noodleTitle":"Fishing with Friends",
         "userID":1,
-        "noodlerName":"Pam Poovey",
+        "userName":"Pam Poovey",
+        "userRating":4,
         "noodleStatus":"event",
         "noodleDescription":"Hey, I am Pam! I love fishing but none of my friends do! I have a boat, and all the gear you could possibly need. Fish is on the menu but if we get skunked, I have food available as well.",
-        "noodleTags":["fishing", "food", "hobby", "friends", "sports"]
+        "noodleTags":["fishing", "food", "hobby", "friends", "sports"],
+        "noodleImage":"fishing"
     },
     {
         "noodleID":4,
         "noodleTitle":"Fishing with Friends",
         "userID":1,
-        "noodlerName":"Pam Poovey",
+        "userName":"Pam Poovey",
+        "userRating":4,
         "noodleStatus":"dream",
         "noodleDescription":"Hey, I am Pam! I love fishing but none of my friends do! I have a boat, and all the gear you could possibly need. Fish is on the menu but if we get skunked, I have food available as well.",
-        "noodleTags":["fishing", "food", "hobby", "friends", "sports"]
+        "noodleTags":["fishing", "food", "hobby", "friends", "sports"],
+        "noodleImage":"fishing"
     },
     {
         "noodleID":5,
         "noodleTitle":"Fishing with Friends",
         "userID":1,
-        "noodlerName":"Pam Poovey",
+        "userName":"Pam Poovey",
+        "userRating":4,
         "noodleStatus":"event",
         "noodleDescription":"Hey, I am Pam! I love fishing but none of my friends do! I have a boat, and all the gear you could possibly need. Fish is on the menu but if we get skunked, I have food available as well.",
-        "noodleTags":["fishing", "food", "hobby", "friends", "sports"]
+        "noodleTags":["fishing", "food", "hobby", "friends", "sports"],
+        "noodleImage":"fishing"
+    },
+    {
+        "noodleID":6,
+        "noodleTitle":"Fishing with Friends",
+        "userID":1,
+        "userName":"Pam Poovey",
+        "userRating":4,
+        "noodleStatus":"dream",
+        "noodleDescription":"Hey, I am Pam! I love fishing but none of my friends do! I have a boat, and all the gear you could possibly need. Fish is on the menu but if we get skunked, I have food available as well.",
+        "noodleTags":["fishing", "food", "hobby", "friends", "sports"],
+        "noodleImage":"fishing"
+    },
+    {
+        "noodleID":7,
+        "noodleTitle":"Fishing with Friends",
+        "userID":1,
+        "userName":"Pam Poovey",
+        "userRating":4,
+        "noodleStatus":"dream",
+        "noodleDescription":"Hey, I am Pam! I love fishing but none of my friends do! I have a boat, and all the gear you could possibly need. Fish is on the menu but if we get skunked, I have food available as well.",
+        "noodleTags":["fishing", "food", "hobby", "friends", "sports"],
+        "noodleImage":"fishing"
+    },
+    {
+        "noodleID":8,
+        "noodleTitle":"Fishing with Friends",
+        "userID":1,
+        "userName":"Pam Poovey",
+        "userRating":4,
+        "noodleStatus":"event",
+        "noodleDescription":"Hey, I am Pam! I love fishing but none of my friends do! I have a boat, and all the gear you could possibly need. Fish is on the menu but if we get skunked, I have food available as well.",
+        "noodleTags":["fishing", "food", "hobby", "friends", "sports"],
+        "noodleImage":"fishing"
+    },
+    {
+        "noodleID":9,
+        "noodleTitle":"Fishing with Friends",
+        "userID":1,
+        "userName":"Pam Poovey",
+        "userRating":4,
+        "noodleStatus":"dream",
+        "noodleDescription":"Hey, I am Pam! I love fishing but none of my friends do! I have a boat, and all the gear you could possibly need. Fish is on the menu but if we get skunked, I have food available as well.",
+        "noodleTags":["fishing", "food", "hobby", "friends", "sports"],
+        "noodleImage":"fishing"
+    },
+    {
+        "noodleID":10,
+        "noodleTitle":"Fishing with Friends",
+        "userID":1,
+        "userName":"Pam Poovey",
+        "userRating":4,
+        "noodleStatus":"event",
+        "noodleDescription":"Hey, I am Pam! I love fishing but none of my friends do! I have a boat, and all the gear you could possibly need. Fish is on the menu but if we get skunked, I have food available as well.",
+        "noodleTags":["fishing", "food", "hobby", "friends", "sports"],
+        "noodleImage":"fishing"
+    }
+]
+
+export var userData = [
+    {
+        "userID":1,
+        "userName":"Pam Poovey",
+        "userRating":4
     }
 ]
