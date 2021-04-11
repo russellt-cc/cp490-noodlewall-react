d18d87d0e5a9a9b831100683fe82f7e2b5cf0c4b
Russell Tennant <rtennant@confederationcollege.ca>
2021-04-11 13:08:12 -0400

    Cleaned up browse.js. Added filter by type to NoodleList.js.

--- src/Browse/Browse.js
+++ src/Browse/Browse.js
@@ -4,27 +4,14 @@ import NoodleList from './NoodleList.js'
 
 class Browse extends React.Component {
   render() {
-    // Testing the properties
-    // var type=""
-    // var query=""
-    // var match=""
-    // var location=""
-    // if (this.props.match !== undefined) {
-    //   type = this.props.match.params.type
-    //   match = JSON.stringify(this.props.match, null, 2)
-    // }
-    // if (this.props.location !== undefined) {
-    //   query = this.props.location.search
-    //   location = JSON.stringify(this.props.location, null, 2)
-    // }
     // Getting information about events
     return (
       <main>
-        {/* <p>testing browsing type={type} query={query}</p>
-        <p>Match Props: {match}</p>
-        <p>Location Props: {location}</p>
-        <p>List of Noodles</p> */}
-        <NoodleList/>
+        {/* Show the list of noodles. Pass filters as properties to the NoodleList. */}
+        <NoodleList 
+          query={this.props.location.search}
+          type={this.props.match.params.type}
+        />
       </main>
     );
   }
--- src/Browse/NoodleList.js
+++ src/Browse/NoodleList.js
@@ -11,14 +11,40 @@ import NoodleCard from "./NoodleCard.js"
 // Could be modified to pull data from a database
 class NoodleList extends React.Component {
     render() {
+        // Function to determine if the noodle matches the filters
+        function filterNoodles (item, filters) {
+            // Return false if the type doesn't match
+            switch (filters.type) {
+                case "dreams":
+                    if (item.noodleStatus !== "dream") {
+                        return false
+                    }
+                    break
+                case "events":
+                    if (item.noodleStatus !== "event") {
+                        return false
+                    }
+                    break
+                default:
+                    break
+                }
+            // If the filters match, return the data
+            return true
+        }
+        // Create the noodle list and start looping through entries
         return(
             // div for the list of dreams and events
             <div className="noodle_list">
-                {/* Mapping array of objects */}
-                {noodleData.map ((item, i) => (
-                    // create a noodle object for each JSON item
-                    <NoodleCard data={item} />
-                ))}
+                {/* Mapping array of objects.
+                Send the data to filter noodles function
+                to return the data or not
+                depending on filters. */}
+                {noodleData.map ((item, i) => {
+                    // If the filters match, return the data
+                    if (filterNoodles(item, this.props)){
+                        return <NoodleCard data={item} index={i}/>
+                    }
+                })}
             </div>
         )
     }
