68a246bb7af8fc5521f4aec7537c8a19da26ed9e
Russell Tennant <rtennant@confederationcollege.ca>
2021-04-11 15:40:40 -0400

    Added intro to details page.

--- src/Details/Details.js
+++ src/Details/Details.js
@@ -1,22 +1,54 @@
 import './css/Details.css';
 import React from 'react';
-import { noodleData } from "../noodleData.js"
+import { noodleData, userData } from "../noodleData.js"
+import { Link } from 'react-router-dom';
+import fishing from "../images/fishing-crop.png"
 
 class Details extends React.Component {
   render() {
     // Get the JSON from the file
     // Replace with database query
     const noodle = noodleData[this.props.match.params.id - 1]
+    // Get the host details
+    // Replace with database query
+    const host = userData[noodle.userID - 1]
     // Return the details page
     return (
       <main>
 
-        <section id="intro">
-          <h1>{noodle.noodleTitle}</h1>
-
+        <section id="details_intro">
+          <div id="details_intro_left" className="details_intro_column">
+            <h1>{noodle.noodleTitle}</h1>
+            <p>Days Left to Make it Happen: </p>
+            <img src={fishing} alt="Noodle"></img>
+            <p>Status bar goes here!</p>
+          </div>
+          <div id="details_intro_right" className="details_intro_column">
+            <h1>Event Details</h1>
+            <div id="details_summary">
+              <p>Location: {noodle.noodleLocation}</p>
+              <p>Date and Time: {noodle.noodleDate}</p>
+              <p>{noodle.noodleTime}</p>
+              <p>Ticket Price: {noodle.noodlePrice}</p>
+              <p>Minimum Tickets Sold: {noodle.noodleMinTickets}</p>
+              <p>Maximum Tickets Sold: {noodle.noodleMaxTickets}</p>
+              <div id="details_tags">
+                {noodle.noodleTags.map(item => {
+                  // create a link for each tag
+                  return <Link className={`noodle_tag ${noodle.noodleStatus}_tag`} to={`/browse/${item}`}>#{item}</Link>
+                })}
+              </div>
+            </div>
+            <div id="details_host_intro">
+              <p>Host: {host.userName}</p>
+              <Link to={`/user/${host.userID}/contact`}>Contact {host.userName}</Link>
+              <Link to={`/user/${host.userID}/follow`}>Follow {host.userName}</Link>
+              <Link id="details_buy_button" to={`/buy/${noodle.noodleID}`}>Buy a Ticket</Link>
+            </div>
+          </div>
         </section>
 
-        <section id="details">
+        <section id="details_details">
 
         </section>
 
--- src/noodleData.js
+++ src/noodleData.js
@@ -26,7 +26,14 @@ export const noodleData = [
     "noodleStatus": "event",
     "noodleDescription": "Hey, I am Pam! I love fishing but none of my friends do! I have a boat, and all the gear you could possibly need. Fish is on the menu but if we get skunked, I have food available as well.",
     "noodleTags": ["fishing", "food", "hobby", "friends", "sports"],
-    "noodleImage": "fishing"
+    "noodleImage": "fishing",
+    "noodleLocation": "Sydney, Ontario",
+    "noodleDate": "2021-04-01",
+    "noodleTime": "8:30AM-5:00PM",
+    "noodlePrice": "$17.00",
+    "noodleMinTickets": 2,
+    "noodleMaxTickets": 3,
+    "noodleTicketsSold": 1,
   },
   {
     "noodleID": 4,
@@ -44,7 +51,14 @@ export const noodleData = [
     "noodleStatus": "event",
     "noodleDescription": "Hey, I am Pam! I love fishing but none of my friends do! I have a boat, and all the gear you could possibly need. Fish is on the menu but if we get skunked, I have food available as well.",
     "noodleTags": ["fishing", "food", "hobby", "friends", "sports"],
-    "noodleImage": "fishing"
+    "noodleImage": "fishing",
+    "noodleLocation": "Sydney, Ontario",
+    "noodleDate": "2021-04-01",
+    "noodleTime": "8:30AM-5:00PM",
+    "noodlePrice": "$17.00",
+    "noodleMinTickets": 2,
+    "noodleMaxTickets": 3,
+    "noodleTicketsSold": 1,
   },
   {
     "noodleID": 6,
@@ -71,7 +85,14 @@ export const noodleData = [
     "noodleStatus": "event",
     "noodleDescription": "Hey, I am Pam! I love fishing but none of my friends do! I have a boat, and all the gear you could possibly need. Fish is on the menu but if we get skunked, I have food available as well.",
     "noodleTags": ["fishing", "food", "hobby", "friends", "sports"],
-    "noodleImage": "fishing"
+    "noodleImage": "fishing",
+    "noodleLocation": "Sydney, Ontario",
+    "noodleDate": "2021-04-01",
+    "noodleTime": "8:30AM-5:00PM",
+    "noodlePrice": "$17.00",
+    "noodleMinTickets": 2,
+    "noodleMaxTickets": 3,
+    "noodleTicketsSold": 1,
   },
   {
     "noodleID": 9,
@@ -89,7 +110,14 @@ export const noodleData = [
     "noodleStatus": "event",
     "noodleDescription": "Hey, I am Pam! I love fishing but none of my friends do! I have a boat, and all the gear you could possibly need. Fish is on the menu but if we get skunked, I have food available as well.",
     "noodleTags": ["fishing", "food", "hobby", "friends", "sports"],
-    "noodleImage": "fishing"
+    "noodleImage": "fishing",
+    "noodleLocation": "Sydney, Ontario",
+    "noodleDate": "2021-04-01",
+    "noodleTime": "8:30AM-5:00PM",
+    "noodlePrice": "$17.00",
+    "noodleMinTickets": 2,
+    "noodleMaxTickets": 3,
+    "noodleTicketsSold": 1,
   }
 ]
 
