40dbd1793b988d5b69ad398146e4c4404eb1bce2
Russell Tennant <rtennant@confederationcollege.ca>
2021-04-14 13:42:13 -0400

    Added more sections to create form

--- src/Create/Create.js
+++ src/Create/Create.js
@@ -67,6 +67,7 @@ class Create extends React.Component {
           </section>
           <section id="date_time">
             <h1>Date and Time</h1>
+            <p>Set the date and time of the event.</p>
             <div>
               <label for="eventDate">Event Date</label>
               <input type="date" name="eventDate"></input>
@@ -76,9 +77,37 @@ class Create extends React.Component {
               <input type="time" name="eventTime"></input>
             </div>
           </section>
-          <section id="images"></section>
-          <section id="tickets"></section>
-          <section id="make_it_happen"></section>
+          <section id="images">
+            <h1>Upload Images</h1>
+            <p>
+              Upload as many images as you would like to be displayed on the
+              event page.
+            </p>
+            <div>
+              <label for="eventImage">Event Image</label>
+              <input type="file" accept="image/*" name="eventImage"></input>
+            </div>
+          </section>
+          <section id="tickets">
+            <h1>Create Tickets</h1>
+            <p>Create ticket types available for the event.</p>
+            <div>
+              <label for="eventTicketPrice">Ticket Price</label>
+              <input type="number" name="eventTicketPrice"></input>
+            </div>
+          </section>
+          <section id="secret_sauce">
+            <h1>Secret Sauce</h1>
+            <p>Adjust the secret sauce details to make the noodle stick.</p>
+            <div>
+              <label for="eventMinTickets">Minimum Tickets Required</label>
+              <input type="number" name="eventMinTickets"></input>
+              <label for="eventMaxTickets">Maximum Tickets Available</label>
+              <input type="number" name="eventMaxTickets"></input>
+              <label for="eventCutoff">Cutoff Date</label>
+              <input type="date" name="eventCutoff"></input>
+            </div>
+          </section>
         </form>
       </main>
     );
