e94cca8e14085b79696be49248308e1aa7f38b37
Russell Tennant <rtennant@confederationcollege.ca>
2021-04-20 15:39:07 -0400

    Added minimize button to create nav progress bar. Moved css to its own file.

--- src/Create/CreateNavProgressBar.js
+++ src/Create/CreateNavProgressBar.js
@@ -1,12 +1,19 @@
 import React from "react";
 
+import "./CreateNavProgressBar.css";
+
 // The nav / progress bar shown on top of the create page
 class CreateNavProgressBar extends React.Component {
   render() {
     const { createMode, sections } = this.props;
     if (createMode === "event") {
       return (
-        <div id="create_nav_progress_bar">
+        <div
+          id="create_nav_progress_bar"
+          className={
+            this.state && this.state.minimized ? "minimized" : "maximized"
+          }
+        >
           <div
             id="create_nav_progress_bar_line"
             className={`${
@@ -65,6 +72,22 @@ class CreateNavProgressBar extends React.Component {
               <p>{sections[7 - 1].name}</p>
             </div>
           </div>
+          <button
+            type="button"
+            id="create_nav_progress_bar_mimimize"
+            className="create_nav_progress_bar_toggle"
+            onClick={() => this.setState({ minimized: true })}
+          >
+            -
+          </button>
+          <button
+            type="button"
+            id="create_nav_progress_bar_maximize"
+            className="create_nav_progress_bar_toggle"
+            onClick={() => this.setState({ minimized: false })}
+          >
+            +
+          </button>
         </div>
       );
     } else {
