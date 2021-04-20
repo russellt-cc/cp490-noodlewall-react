0973c4e030ce29ff7606d900395b7c16f377cafc
Russell Tennant <rtennant@confederationcollege.ca>
2021-04-20 14:18:19 -0400

    Added image gallery to details.

--- src/Details/DetailsIntro/DetailsIntroLeft.js
+++ src/Details/DetailsIntro/DetailsIntroLeft.js
@@ -1,7 +1,17 @@
+// React
 import React from "react";
+
+// Noodlewall Components
 import NoodleOverlay from "../../Common/NoodleOverlay";
-import { DateTime, Interval } from "luxon";
 import DetailsProgress from "./DetailsProgress";
+
+// https://moment.github.io/luxon/
+// https://www.npmjs.com/package/luxon
+import { DateTime, Interval } from "luxon";
+
+// https://www.npmjs.com/package/react-image-gallery
+// https://www.linxtion.com/demo/react-image-gallery/
+import ImageGallery from "react-image-gallery";
 
 // The left column of the details intro
 class DetailsIntroLeft extends React.Component {
@@ -48,7 +58,17 @@ class DetailsIntroLeft extends React.Component {
       noodleMinTickets,
       noodleCoverImage,
       noodleStatus,
+      noodleImages,
     } = this.props.thisNoodle;
+
+    // Images for image gallery
+    const getImages = () => {
+      const images = noodleImages.map((image, index) => {
+        return { original: decodeURIComponent(image) };
+      });
+      return images;
+    };
+
     return (
       <div id="details_intro_left" className="details_column left">
         <h1>{noodleTitle}</h1>
@@ -71,14 +91,31 @@ class DetailsIntroLeft extends React.Component {
             <strong>{wholeDaysLeft}</strong>
           </span>
         </p>
-        {noodleCoverImage ? (
+        {/* {noodleCoverImage ? (
           <div id="details_image_container">
             <img src={decodeURIComponent(noodleCoverImage)} alt="Noodle"></img>
             {NoodleOverlay(noodleStatus)}
           </div>
         ) : (
           <></>
-        )}
+        )} */}
+        <div id="details_image_container">
+          <ImageGallery
+            items={getImages()}
+            showThumbnails={false}
+            showPlayButton={false}
+            showFullscreenButton={false}
+            showBullets={noodleImages[1] ? true : false}
+            showNav={noodleImages[1] ? true : false}
+            onErrorImageURL={
+              "http://gatkinson.site/noodlewall/images/site/noodlewall-video-poster-384p.png"
+            }
+            startIndex={noodleImages.indexOf(noodleCoverImage)}
+            autoPlay={false}
+            lazyLoad={true}
+          />
+          {NoodleOverlay(noodleStatus)}
+        </div>
         <DetailsProgress
           thisNoodle={this.props.thisNoodle}
           status_classes={status_classes}
